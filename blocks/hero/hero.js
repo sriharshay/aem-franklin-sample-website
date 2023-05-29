b22ec95b99b4ee78c463001783b69677a5bd8c20
Sriharsha <Sriharsha.Yaratapalli@alliancedata.com>
2023-05-29 21:44:44 +0530

    fix list issues

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,7 +1,8 @@
 export default function decorate(block) {
-    const h1 = block.querySelector('h1');
-    if (h1) {
-        const h1Text = h1.innerText;
-        h1.innerText = h1Text + '\nText from Hero block scrip, have fun..!';
-    }
+  const h1 = block.querySelector('h1');
+  const text = '\nText from Hero block scrip, have fun..!';
+  if (h1) {
+    const h1Text = h1.innerText;
+    h1.innerText = h1Text + `${text}`;
+  }
 }
